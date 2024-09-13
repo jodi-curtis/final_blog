fb4ade4788fce5cad275f037e5262334f457abb5
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-09-13 23:39:47 +0100

    Update Database Variable

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
+SQLALCHEMY_DATABASE_URI = os.getenv('RENDER_DATABASE_URL', 'sqlite:///db.sqlite3')
